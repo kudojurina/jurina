fe6787767e241d141b8c5839106324ca390e0574
kudojurina <167832586+kudojurina@users.noreply.github.com>
2025-01-13 20:58:35 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,6 +1,6 @@
 function imgchange1() {
-    document.getElementById('マルコ').src = './image/2.jpeg';
+    document.getElementById('マルコ').src = './image/2.jpg';
 }
 function imgchange2() {
-    document.getElementById('マルコ').src = './image/1.jpeg';
+    document.getElementById('マルコ').src = './image/1.jpg';
 }
